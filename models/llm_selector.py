9a4540bdec795477cec7f320b07a467600fe72c6
alfredo-sampron <alfredo.sampron@despegar.com>
2024-10-13 18:52:46 -0300

    access llama through HF

--- models/llm_selector.py
+++ models/llm_selector.py
@@ -1,17 +1,17 @@
 import os
 from dotenv import load_dotenv
-from langchain.llms import LLaMA, HuggingFaceHub
+from langchain.llms import HuggingFaceHub
 from langchain import PromptTemplate, LLMChain
 import config
 
-load_dotenv('../../.env')
+load_dotenv()
 
 api_token = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
 
 class LLMSelector:
     def __init__(self, tech_config={}, nontech_config={}):
         self.techllm = HuggingFaceHub(repo_id="bigscience/bloom-560m", model_kwargs=tech_config, huggingfacehub_api_token=api_token)
-        self.nontechllm = LLaMA(**nontech_config)
+        self.nontechllm = HuggingFaceHub(repo_id="meta-llama/Meta-Llama-3-8B", model_kwargs=nontech_config, huggingfacehub_api_token=api_token)
         self.technical_chain = LLMChain(
             llm=self.techllm,
             prompt=PromptTemplate(template=config.TECHNICAL_PROMPT, input_variables=["context", "question"])
