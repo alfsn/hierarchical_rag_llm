d988fb0f184c4938aa62e94224f9ff9bca9d4f03
alfredo-sampron <58914132+alfsn@users.noreply.github.com>
2024-10-13 14:22:00 -0300

    use the same llm to summarize

--- models/summarization.py
+++ models/summarization.py
@@ -1,10 +1,13 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
+from langchain import PromptTemplate
+import config
 
 class HierarchicalSummarizer:
-    def __init__(self, llm=None):
+    def __init__(self, llm):
         self.llm = llm
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        self.summary_prompt = PromptTemplate(template=config.SUMMARY_PROMPT, input_variables=["text"])
 
     def summarize(self, document):
         texts = self.text_splitter.split_text(document)
@@ -12,7 +15,7 @@ class HierarchicalSummarizer:
         if len(texts) == 1:
             return texts[0]
         
-        chain = load_summarize_chain(self.llm, chain_type="map_reduce")
+        chain = load_summarize_chain(self.llm, chain_type="map_reduce", map_prompt=self.summary_prompt, combine_prompt=self.summary_prompt)
         summary = chain.run(texts)
         
         return summary
